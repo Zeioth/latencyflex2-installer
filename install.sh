3311dbe06c899569c75e6e9815477b3e0cb7b404
Zeioth <Zeioth@hotmail.com>
2023-04-13 16:36:03 +0200

    Fix: Tell package-release.sh to use branch 'lfx2-v2.0.0-alpha.2' instead of 'master'

--- install.sh
+++ install.sh
@@ -28,21 +28,15 @@ cd ../..
 
 # Compiling the DXVK fork
 git clone --recursive https://github.com/ishitatsuyuki/dxvk.git -b lfx2-v2.0.0-alpha.2
-cd dxvk
-./package-release.sh master ./target --no-package
-cd ..
+./dxvk/package-release.sh lfx2-v2.0.0-alpha.2 ./target --no-package
 
 # Compiling the DXVK-NVAPI fork
 git clone --recursive https://github.com/ishitatsuyuki/dxvk-nvapi.git -b lfx2-v2.0.0-alpha.2
-cd ./dxvk-nvapi
-./package-release.sh master ./target
-cd ..
+./dxvk-nvapi/package-release.sh lfx2-v2.0.0-alpha.2 ./target
 
 # Compiling the VKD3D-Proton fork
 git clone --recursive https://github.com/ishitatsuyuki/vkd3d-proton.git -b lfx2-v2.0.0-alpha.2
-cd ./vkd3d-proton
-./package-release.sh master ./target --no-package
-cd ..
+./vkd3d-proton/package-release.sh lfx2-v2.0.0-alpha.2 ./target --no-package
 
 
 
@@ -59,19 +53,19 @@ done
 for PROTON_PATH in ~/.steam/steam/steamapps/common/"Proton - Experimental" ; do
   # Install the DXVK fork
   chmod 655 "$PROTON_PATH"/files/lib64/wine/dxvk/*
-  yes | cp ./dxvk/target/dxvk-master/x64/*.dll "$PROTON_PATH/files/lib64/wine/dxvk/"
+  yes | cp ./dxvk/target/dxvk-lfx2-v2.0.0-alpha.2/x64/*.dll "$PROTON_PATH/files/lib64/wine/dxvk/"
   chmod 555 "$PROTON_PATH"/files/lib64/wine/dxvk/*
   echo "DXVK fork installed in $PROTON_PATH"
 
   # Install dxvk-nvapi fork
   chmod 655 "$PROTON_PATH"/files/lib64/wine/nvapi/*
-  yes | cp ./dxvk-nvapi/target/dxvk-nvapi-master/x64/*.dll "$PROTON_PATH/files/lib64/wine/nvapi/"
+  yes | cp ./dxvk-nvapi/target/dxvk-nvapi-lfx2-v2.0.0-alpha.2/x64/*.dll "$PROTON_PATH/files/lib64/wine/nvapi/"
   chmod 555 "$PROTON_PATH"/files/lib64/wine/nvapi/*
   echo "DXKV-NVAPI installed in $PROTON_PATH"
 
   # Install vkd3d-proton fork
   chmod 655 "$PROTON_PATH"/files/lib64/wine/vkd3d-proton/*
-  yes | cp ./vkd3d-proton/target/vkd3d-proton-master/x64/*.dll "$PROTON_PATH/files/lib64/wine/vkd3d-proton/"
+  yes | cp ./vkd3d-proton/target/vkd3d-proton-lfx2-v2.0.0-alpha.2/x64/*.dll "$PROTON_PATH/files/lib64/wine/vkd3d-proton/"
   chmod 555 "$PROTON_PATH"/files/lib64/wine/vkd3d-proton/*
   echo "DXVK-NVAPI installed in $PROTON_PATH"
 done
