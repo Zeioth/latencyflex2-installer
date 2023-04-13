37e8f044ca89168ebc2eac545e8f00e9e2960bd8
Zeioth <Zeioth@hotmail.com>
2023-04-13 20:35:41 +0200

    Shor all games where LatencyFlex2 was installed instead of just Apex

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 # DESCRIPTION: This file installs LatencyFlex2 into Proton Experimental,
 #              and all games currently installed on Steam.
 #
-
+set -eu
 
 
 
@@ -87,8 +87,9 @@ echo "dxgi.customVendorId = 10de" > ~/.local/share/latency-flex/dxvk.conf
 
 
 ##### SHOW INSTALLED FILES #####
-
-ls -l ~/.steam/steam/steamapps/compatdata/1172470/pfx/drive_c/windows/system32/ | grep latencyflex2_rust.dll
+for GAME in ~/.steam/steam/steamapps/compatdata/* ; do
+  ls -l "$GAME"/pfx/drive_c/windows/system32/ | grep latencyflex2_rust.dll
+done
 ls -I openvr_api_dxvk.dll -I version -l ~/.steam/steam/steamapps/common/"Proton - Experimental"/files/lib64/wine/dxvk
 ls -I version -l ~/.steam/steam/steamapps/common/"Proton - Experimental"/files/lib64/wine/nvapi
 ls -I version -I d3d12core.dll -l ~/.steam/steam/steamapps/common/"Proton - Experimental"/files/lib64/wine/vkd3d-proton
